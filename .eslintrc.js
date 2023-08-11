ba42f04814480296e8a12075f61c41bc67e6dac5
Jens <github@duttke.de>
2023-08-11 20:15:05 +0200

    Disable rules which enforce syntax of new Node.js versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,9 @@ module.exports = {
 		require.resolve('linter-bundle/eslint/overrides-javascript-lazy'),
 		require.resolve('linter-bundle/eslint/overrides-jsdoc'),
 		require.resolve('linter-bundle/eslint/overrides-type-declarations')
-	]
+	],
+	rules: {
+		'@typescript-eslint/prefer-nullish-coalescing': 'off', // Disabled as we are supporting Node.js v10, which does not support Nullish Coalescing
+		'@typescript-eslint/prefer-optional-chain': 'off' // Disabled as we are supporting Node.js v10, which does not support Nullish Coalescing
+	}
 };
