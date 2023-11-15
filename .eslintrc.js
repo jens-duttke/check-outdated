7922b543c723899dce79470c5b5045f45ad7d7f6
Jens <github@duttke.de>
2023-11-15 12:30:54 +0100

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@
 
 module.exports = {
 	extends: [
-		require.resolve('linter-bundle/eslint'),
-		require.resolve('linter-bundle/eslint/overrides-javascript-lazy'),
-		require.resolve('linter-bundle/eslint/overrides-jsdoc'),
-		require.resolve('linter-bundle/eslint/overrides-type-declarations')
+		require.resolve('linter-bundle/eslint.cjs'),
+		require.resolve('linter-bundle/eslint/overrides-javascript-lazy.cjs'),
+		require.resolve('linter-bundle/eslint/overrides-jsdoc.cjs'),
+		require.resolve('linter-bundle/eslint/overrides-type-declarations.cjs')
 	],
 	rules: {
 		'@typescript-eslint/prefer-nullish-coalescing': 'off', // Disabled as we are supporting Node.js v10, which does not support Nullish Coalescing
